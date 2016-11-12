1313f2d36f3aacfe7507fcfadfdea1020d1d64ae
Irshad <irshadpi77@gmail.com>
2016-11-12 16:56:31 +0530

    Article Fetching added

--- fb_bot/models.py
+++ fb_bot/models.py
@@ -9,8 +9,26 @@ class Subscriber(models.Model):
 	subscriptions = models.CharField(max_length = 200,default="")
 	read_list = models.CharField(max_length = 200,default="") 
 	is_subscribed = models.BooleanField(default = False)
-	preferred_lang = models.CharField(default = "en")
+	preferred_lang = models.CharField(default = "en",max_length=10)
 	def __unicode__(self):
 		return self.name + self.subscriptions
+
+
 class Section(models.Model):
+	code = models.CharField(max_length = 200,unique=True)
 	name = models.CharField(max_length = 200,default = "")
+	title = models.CharField(max_length = 200,default = "")
+	category = models.CharField(max_length = 200,default = "")
+	parent = models.CharField(max_length = 200,default = "")
+	def __unicode__(self):
+		return self.code +" "+ self.title
+
+class Article(models.Model):
+	articleID = models.CharField(max_length = 200,unique=True)
+	title = models.CharField(max_length = 200,default = "")
+	articleURL = models.CharField(max_length = 300,default = "")
+	thumbnail = models.CharField(max_length = 400,default = "")
+	summary = models.CharField(max_length = 400,default = "")
+	section = models.ForeignKey(Section)
+	def __unicode__(self):
+		return self.title 
--- fb_bot/views.py
+++ fb_bot/views.py
@@ -11,10 +11,13 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from .models import *
 
+
 NEWS_SECTIONS = ['wellness','top-stories','business','editors-pick',
 'columns','multimedia','currency','life-style','entertainment',
 'sports','news','in-depth']
 
+
+GREETINGS = ["hi","hello","hey","hai"]
 # UTILITY FUNCTIONS
 
 # creates a new user if he is messaging for first time.
@@ -49,16 +52,39 @@ def post_facebook_buttons(fbid,buttonarray):
             "attachment":{"type":"template",
                 "payload":{"template_type":"button",
                 "text":"Do you want to subscribe to our daily news letter",
-                "buttons":[
-                    buttonarray
-                    ]}
+                "buttons":buttonarray
+                }
             }
         }
     })
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     pprint(status.json())  
 
+def post_list(fbid,articles):
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAACZCBHTxqYQBAL0fqNJOmKLJEunGZCrLEo38r5oSyjoNFi2I1mGmqOQ9pjXPVtZBGDNZBQdYVHR21L655La3ZCUBQd2MuqSqYZCZBuU3XZAsEaHqWqHC6uag1ZAgxC5GmAECG2Wfxt120pkIo0A2Yt1atPZAJiV8IevkMr2ZAPooMLnAZDZD' 
+    print(articles)
+    response_msg = json.dumps({"recipient":{"id":fbid}, 
+        "message":{
+            "attachment":{
+                "type":"template",
+                "payload":{
+                    "template_type":"list",
+                    "elements":  [{"buttons": [{"messenger_extensions": True, "title": "Read More", "url": "https://developer.manoramaonline.com/api/editions/en/articles/42b6210a8866f06ede8eb8f074f93068", "webview_height_ratio": "tall", "type": "web_url", "fallback_url": "https://developer.manoramaonline.com/api/editions/en/articles/42b6210a8866f06ede8eb8f074f93068"}], "image_url": "http://english.manoramaonline.com/content/dam/mm/en/entertainment/review/images/2016/Nov/11/achcham-yenbadhu-madamaiyada-review.jpg.image.160.84.png", "default_action": {"url": "https://developer.manoramaonline.com/api/editions/en/articles/42b6210a8866f06ede8eb8f074f93068", "webview_height_ratio": "tall", "messenger_extensions": True, "type": "web_url", "fallback_url": "https://developer.manoramaonline.com/api/editions/en/articles/42b6210a8866f06ede8eb8f074f93068"}, "subtitle": " hrdtrc", "title": "Achcham Yenbadhu Madamaiyada (AYM): audience review & live updates"},]
+                }
+            }
+        }
+    })
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+    pprint(status.json())     
 
+def mark_seen_start_typing(fbid):
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAACZCBHTxqYQBAL0fqNJOmKLJEunGZCrLEo38r5oSyjoNFi2I1mGmqOQ9pjXPVtZBGDNZBQdYVHR21L655La3ZCUBQd2MuqSqYZCZBuU3XZAsEaHqWqHC6uag1ZAgxC5GmAECG2Wfxt120pkIo0A2Yt1atPZAJiV8IevkMr2ZAPooMLnAZDZD' 
+    response_msg = json.dumps({"recipient":{"id":fbid}, "sender_action":"typing_on"})
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+def get_articles(section):
+    r = requests.get('https://developer.manoramaonline.com/api/editions/en/sections/'+section+'/articles?size=1', headers={'Authorization': '34aa328d-8354-50dc-bbeb-da209b13a458'})
+    r = r.json()
+    return (r["articleSummary"])
 # retrns the edit distance between two points
 def edit_distance(s, t):
         ''' From Wikipedia article; Iterative with two matrix rows. '''
@@ -82,24 +108,63 @@ def edit_distance(s, t):
 # used to store sections to database
 def fetch_and_store_sections():
     r = requests.get('https://developer.manoramaonline.com/api/editions/en/sections', headers={'Authorization': '34aa328d-8354-50dc-bbeb-da209b13a458'})
-    return (r)
+    r = r.json()
+    for j in r['sections']:
+        if('sections' in j):
+            for i in j['sections']:
+                try:
+                    section = Section.objects.get(code = i["code"])
+                except:
+                    cat = ''
+                    for j in i['type']:
+                        cat += j+','
+                    print(cat)
+                    section = Section(code=i['code'],name = i['name'],title=i['title'],category=cat,parent = i["parentCode"])
+                    section.save()
+
+def get_fb_array(articles):
+    res = []
+    for i in articles:
+        cur = {
+        'title': i['title'],
+        'image_url': i['thumbnail'],
+        'subtitle': ' ',
+        'default_action' :{
+            "type": "web_url",
+            "url": i["articleURL"],
+            "webview_height_ratio":"tall",
+            "fallback_url": i["articleURL"],
+            "messenger_extensions": True
+
+        },
+        "buttons": [
+                    {
+                        "title": "Read More",
+                        "type": "web_url",
+                        "url": i["articleURL"],
+                        "messenger_extensions": True,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": i["articleURL"]
+                    }
+                ] 
+            }     
+        res.append(cur)
+    print(res)
+    return res          
+
 def get_matching_section(a):
     a = a.lower().replace(' ','-')
     a_len = len(a)
     selected = "none"
     min_point = 3
+    sec =  Section.objects.all()
+    #print(NEWS_SECTIONS)
     for i in NEWS_SECTIONS:
         cur_point = edit_distance(a,i)
         if(cur_point < min_point):
             min_point = cur_point
             selected = i
     return selected
-
-def print_help(fbid):
-    user = Subscriber.objects.get(id = int(fbid))
-    text = "Hi "+user.name +"! \n"
-    text += "Here are some tips\n"
-    text += " use subscribe <topic name> to subscribe to a"
 
 
 
@@ -118,7 +183,7 @@ def post_facebook_subscribe_daily_button(fbid):
             "payload":"subscribe_daily_no"
           }
         ] 
-    post_facebook_buttons(buttonarray)  
+    post_facebook_buttons(fbid,buttonarray)  
 
 # Create your views here.dfs
 class FbBotView(generic.View):
@@ -149,10 +214,25 @@ class FbBotView(generic.View):
                     except:
                         create_user(message['sender']['id'])
 
-                    
+
+                    mark_seen_start_typing(fbid)
                     pprint(message)   
                     if 'text' in message['message']:
-                        if(message['mess'])
+                        is_section = get_matching_section(message['message']['text'])
+                        if(is_section != "none" ):
+                            sections = Section.objects.filter(parent=is_section)
+                            articles = []
+                            for i in sections:
+                                sec = i.code
+                                articles += get_articles(sec)
+
+                            print(articles)
+                            post_list(fbid,get_fb_array(articles))
+                        else:
+                            if(message['message']['text'].lower() in GREETINGS):
+                                post_facebook_message(fbid,"Hello, type in category to get news around you ")
+                            else:
+                                post_facebook_message(fbid,"Sorry, we couldn't understand your request,  type in category to get news around you ")
                     else:
                         post_facebook_message(fbid,"Thank you :) ")   
                 elif 'postback' in message:
